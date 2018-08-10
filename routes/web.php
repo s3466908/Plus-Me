edcfa7026a8cf5beedd52c97944f26554a217ba2
Omer Khan <s3466908@student.rmit.edu.au>
2018-08-10 15:42:12 +1000

    Second commit with google maps api

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,23 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $config['center'] = 'RMIT, Australia';
+    $config['zoom'] = '18';
+    $config['map_height'] = '500px';
+    //$config['map_width'] = '500px';
+    $config['scrollwheel'] = false;
+
+    GMaps::initialize($config);
+
+
+    // Add Marker
+    $marker['position']= 'RMIT,Australia';
+    $marker['infowindow_content']= 'RMIT University Melbourne,Australia';
+    // Using icons from google site at https://sites.google.com/site/gmapsdevelopment/
+    $marker['icon'] ='http://maps.google.com/mapfiles/kml/paddle/blu-circle.png';
+    Gmaps::add_marker($marker);
+
+    $map = GMaps::create_map();
+
+    return view('welcome')->with('map',$map);
 });
