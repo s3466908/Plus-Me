a4ebac9d188440747cc1ee7539585ff789e1914f
StevenHang1994 <36983582+StevenHang1994@users.noreply.github.com>
2018-08-10 22:26:57 +1000

    fixed small errors

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    $config['center'] = 'RMIT, Australia';
+    $config['zoom'] = '18';
+    $config['map_height'] = '500px';
+    //$config['map_width'] = '500px';
+    $config['scrollwheel'] = false;
+
+    GMaps::initialize($config);
+    
+    // Add Marker
+    $marker['position']= 'RMIT,Australia';
+    $marker['infowindow_content']= 'RMIT University Melbourne,Australia';
+    // Using icons from google site at https://sites.google.com/site/gmapsdevelopment/
+    $marker['icon'] ='http://maps.google.com/mapfiles/kml/paddle/blu-circle.png';
+    Gmaps::add_marker($marker);
+
+    $map = GMaps::create_map();
+
+    return view('welcome')->with('map',$map);
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/about', 'PagesController@about')->name('about');
+
+Route::get('/faq', 'PagesController@faq')->name('faq');
+
+Route::get('/policy', 'PagesController@policy')->name('policy');
